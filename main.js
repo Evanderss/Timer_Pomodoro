738fc738ba6d40617a1524396aa6b07a11f87b6d
Evanderss <evanderramirez150@gmail.com>
2022-12-29 16:25:34 -0600

    agregando evento para submit

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ let current = null;
 const badd = document.querySelector("#addi");
 const ittask = document.querySelector("#ittask");
 const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (ittask.value !== "") {
+      createTask(ittask.value);
+      ittask.value = "";
+      renderTasks();
+    }
+  });

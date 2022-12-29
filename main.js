53990190545721b9ae8e248bf9650ef4c694c7ef
Evanderss <evanderramirez150@gmail.com>
2022-12-29 15:57:18 -0600

    incializando variables para el timer

---
+++ main.js
@@ -0,0 +1,9 @@
+const task = [];
+let time = 0;
+let timer = null;
+let timerBreak = null;
+let current = null;
+
+const badd = document.querySelector("#addi");
+const ittask = document.querySelector("#ittask");
+const form = document.querySelector("#form");
